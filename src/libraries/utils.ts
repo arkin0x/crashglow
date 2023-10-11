84ec7fb16b750ed0a3c8e20af71b38703bc13bcd
arkin0x <arkinox@pm.me>
2023-10-10 22:14:09 -0500

    byte order mark? who knew

--- src/libraries/utils.ts
+++ src/libraries/utils.ts
@@ -25,6 +25,15 @@ export const getTagValue = (key: string, value: string): FindTag => {
 // pico-8
 export const replaceScript = (html: string, script: string): string => {
   // replace `` in javascript file
+  /**
+   * TODO: remove byte order mark if it exists
+   * https://stackoverflow.com/questions/24356713/node-js-remove-bom-from-string
+   * GitHub Copilot: The characters `ï»¿` at the beginning of an HTML file are known as the byte order mark (BOM). The BOM is a special marker that is sometimes added to the beginning of a text file to indicate the byte order and encoding of the file.
+
+    In some cases, the BOM can cause issues with the interpretation of the file, especially if the file is being processed by software that does not expect or handle the BOM correctly. This can result in the `ï»¿` characters being displayed at the beginning of the file.
+
+    It's possible that the base64-encoded HTML file you decoded included the BOM at the beginning of the file, which caused the `ï»¿` characters to be displayed when you viewed the decoded file. To remove the BOM from the file, you can try opening the file in a text editor that supports BOM detection and removal, or you can manually remove the BOM using a hex editor or other tool.1
+   */
   const fixedScript = script.replace(/`/g, '\\`')
   const regex = /e\.src\s*=\s*"(.+?)";/
   const match = regex.exec(html)
